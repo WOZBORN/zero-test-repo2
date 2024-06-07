6e11c4865743185b863f7c6b31437d82dfe4138a
Maksim <wolker2015@gmail.com>
2024-06-07 19:21:02 +0700

    changed .gitignore and test.py

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
-inp = input("Введите строку: ")
-words = inp.split()
-print(f"Количество слов в строке: {len(words)}")
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def introduce(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+class Student(Person):
+    def __init__(self, name, age, student_id):
+        super().__init__(name, age)
+        self.student_id = student_id
+
+    def study(self):
+        print(f"{self.name} is now studying.")
+
+
